395330af9c29fd4dde5e98a1a07e03f02e57ebd0
jeogam <jeovanigamati@gmail.com>
2026-01-31 18:30:47 -0300

    ♻️refactor: atualizando health

--- src/main/java/br/com/ifba/gamelog/GameLogApplication.java
+++ src/main/java/br/com/ifba/gamelog/GameLogApplication.java
@@ -2,7 +2,7 @@ package br.com.ifba.gamelog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling; 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
